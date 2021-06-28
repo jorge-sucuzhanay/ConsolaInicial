f03c48930d9b692c27c18fc28871b372d967c0fe
Jorge Sucuzhañay <69119656+jorge-sucuzhanay@users.noreply.github.com>
2021-06-28 12:56:05 -0500

    Añadir archivos

--- ContadorHojas.cs
+++ ContadorHojas.cs
@@ -35,6 +35,50 @@ namespace Arboles
             }
             return i;
         }
+        public string ArbolInfijo(Nodo principal)
+        {
+            if (!principal.Hijos.Any())
+                return principal.Valor;
+
+            return $"( {ArbolInfijo(principal.Hijos[0])} {principal.Valor} {ArbolInfijo(principal.Hijos[1])})";
+
+        }
+        public string ArbolSegundoInfijo(Nodo principal)
+        {
+            if (!principal.Hijos.Any())
+                return principal.Valor;
+
+            return $"( {ArbolSegundoInfijo(principal.Hijos[1])} {principal.Valor} {ArbolSegundoInfijo(principal.Hijos[0])})";
+
+        }
+        public string ArbolPrefijo(Nodo principal)
+        {
+            if (!principal.Hijos.Any())
+                return principal.Valor;
+
+            return $" {principal.Valor}({ArbolPrefijo(principal.Hijos[0])}  {ArbolPrefijo(principal.Hijos[1])})";
+
+        }
+        public string Arbol2Prefijo(Nodo principal)
+        {
+            if (!principal.Hijos.Any())
+                return principal.Valor;
+
+            return $" {principal.Valor}({Arbol2Prefijo(principal.Hijos[1])}  {Arbol2Prefijo(principal.Hijos[0])})";
+
+        }
+        public string Arbol3Prefijo(Nodo principal)
+        {
+            if (!principal.Hijos.Any())
+                return principal.Valor;
+
+            return $" ({Arbol3Prefijo(principal.Hijos[1])}  {Arbol3Prefijo(principal.Hijos[0])}){principal.Valor}";
+
+        }
+
+
+
+
 
     }
 
--- Program.cs
+++ Program.cs
@@ -63,6 +63,13 @@ namespace Arboles
             Console.WriteLine($"El número de hojas del árbol son: {verificadorArbol.CalcularHojas(raiz)}");
             Console.WriteLine($"El número de nodos del árbol son: {verificadorArbol.CalcularNodo(raiz)}");
             Console.WriteLine($"El número de niveles del árbol son: {verificadorArbol.CalcularNiveles(raiz)}");
+            Console.WriteLine("Las expresiones algebraicas son las siguientes: ");
+            Console.WriteLine(verificadorArbol.ArbolInfijo(raiz));
+            Console.WriteLine(verificadorArbol.ArbolSegundoInfijo(raiz));
+            Console.WriteLine(verificadorArbol.ArbolPrefijo(raiz));
+            Console.WriteLine(verificadorArbol.Arbol2Prefijo(raiz));
+            Console.WriteLine(verificadorArbol.Arbol3Prefijo(raiz));
+
 
         }
     }
