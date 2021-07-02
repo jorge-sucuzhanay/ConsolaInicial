124d4ed56722e3e8ff7b154de64a89b9776436ae
Jorge Sucuzhañay <69119656+jorge-sucuzhanay@users.noreply.github.com>
2021-07-01 21:44:33 -0500

    Añadir Archivos

---
+++ Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MiPrimerApp
+{
+    internal class Persona
+    {
+        public string Nombre { get { return _nombre1; } set { _nombre1 = value; } }
+        public string Apellido { get; set; }
+        public string estado1;
+        public string _nombre1;
+        public string GetEstado() { return estado1; }
+        public void SetEstado(string estado) { this.estado1 = estado; }
+        public string Saludar()
+        {
+            return $"Hola me llamo: {this.Nombre}, espero que te encuentres bien";
+        }
+       public void Dormir() { estado1 = "Estoy durmiendo"; Console.WriteLine(estado1); }
+
+}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MiPrimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Stalyn";
+            persona.Apellido = "Vasquez";
+            persona.Saludar();
+            Console.WriteLine("Su nombre es:" + persona.Nombre);
+            Console.WriteLine("Su apellido es:" + persona.Apellido);
+            Console.WriteLine(persona.Saludar());
+            persona.Dormir();
+
+
+
+
+        }
+    }
+}
